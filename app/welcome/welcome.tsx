74a9e7662474e3225e16a6d9948cce2950e9bf4c
MausO-git <oscarmaus01@gmail.com>
2026-05-19 15:20:42 +0200

    Test modif 2

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -24,7 +24,7 @@ export function Welcome() {
             <p className="leading-6 text-gray-700 dark:text-gray-200 text-center">
               What&apos;s next? Test 2
             </p>
-            <h1>Modif</h1>
+            <h1>Modif Test 2</h1>
             <ul>
               {resources.map(({ href, text, icon }) => (
                 <li key={href}>
